677899c343a07519506e4f199f05e9763cd1172c
camili <camili.costa07@gmail.com>
2024-06-06 20:43:04 -0300

    falta finalizar

---
+++ aula06/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using aula06.___;
+
+
+
+// using (var db = new DbLivrariaContext())
+// {
+// var autor = db.TbAutor.Find(10);
+
+// if (autor != null)
+// {
+//     Console.WriteLine ($"o autor é {autor.Nome}");
+// }
+// else
+//     {
+//         Console.WriteLine (" Autor não consta no banco");
+//     }
+// }
+
+using (var db = new DbLivrariaContext()) {
+    var novoAutor = new TbAutor
+    {
+        Nome = "Clarice Lispector",
+        NrFone = "123456789012",
+        Pais = "Ucrânia"
+    };
+    db.TbAutor.Add(novoAutor);
+ db.SaveChanges ();
+}
+
+
---
+++ aula06/___/TbCategoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aula06.___;
+
+public partial class TbCategoria
+{
+    public int IdCategoria { get; set; }
+
+    public string? NmCategoria { get; set; }
+
+    public string? DsCategoria { get; set; }
+
+    public virtual ICollection<TbLivro> TbLivro { get; set; } = new List<TbLivro>();
+}
